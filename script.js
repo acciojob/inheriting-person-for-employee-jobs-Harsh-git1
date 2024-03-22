8cba364169731de69b4b030f282fc474298675d6
Harsh-git1 <130690167+Harsh-git1@users.noreply.github.com>
2024-03-22 22:52:29 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,23 @@
-// complete this js code
-function Person(name, age) {}
+class Person {
+	constructor(name, age) {
+		this.n = name;
+		this.a = age;
+	}
+	greet() {
+		console.log(`Hello, my name is ${this.name}, I am ${this.age} years old.`);
+	}
+}
 
-function Employee(name, age, jobTitle) {}
+class Employee extends Person {
+	constructor(name, age, jobTitle) {
+		super(name, age);
+        this.j = jobTitle;
+	}
+	jobGreet() {
+		console.log(`Hello, my name is ${this.n}, I am ${this.a} years old, and my job title is ${this.j}`);
+	}
+}
+
 
 // Do not change code below this line
 window.Person = Person;
